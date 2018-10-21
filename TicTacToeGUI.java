4d574babc6508c82668ad7837e436fb9ff829e37
Dmitry1490 <Unamas@mail.ru>
2018-10-21 19:50:01 +0300

    Create main class TicTacToeGUI;

---
+++ TicTacToeGUI.java
@@ -0,0 +1,7 @@
+// Полностью разобрался с кодом. Попробовал полнустью переписать его;
+
+public class TicTacToeGUI {
+    public static void main(String[] args){
+        new GameWindow();
+    }
+}
