22befe6cae7a7b435054fa3b1e236c102ccc32e0
Dmitry1490 <Unamas@mail.ru>
2019-08-18 17:00:17 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,16 +1,17 @@
-import java.io.CharArrayReader;
-import java.util.concurrent.Semaphore;
+
+import java.util.concurrent.CountDownLatch;
 
 public class Car implements Runnable {
     private static volatile Integer CARS_COUNT;
-    private Integer START_CARS_COUNT;
+    private static volatile Integer START_CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
     private Race race;
     private int speed;
     private String name;
-    private Semaphore semaphore;
+    //private Semaphore semaphore;
+    private CountDownLatch countDownLatch;
     private boolean Win;
 
     public String getName() {
@@ -20,10 +21,10 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed, Semaphore semaphore) {
+    public Car(Race race, int speed, CountDownLatch countDownLatch) {
         this.race = race;
         this.speed = speed;
-        this.semaphore = semaphore;
+        this.countDownLatch = countDownLatch;
         CARS_COUNT++;
         this.START_CARS_COUNT = CARS_COUNT;
         this.name = "Участник #" + CARS_COUNT;
@@ -31,19 +32,30 @@ public class Car implements Runnable {
     @Override
     public void run() {
         try {
-            semaphore.acquire();
+            countDownLatch.countDown();
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
+            countDownLatch.countDown();
+            Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        countDownLatch.countDown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
         synchronized (CARS_COUNT){
             CARS_COUNT--;
-            if(START_CARS_COUNT-CARS_COUNT == 1){System.out.println(this.name + " Win!");}
+            if(START_CARS_COUNT - CARS_COUNT == 1){System.out.println(this.name + " Win!");}
             if(CARS_COUNT==0){System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");}
         }
     }
